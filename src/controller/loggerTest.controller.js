1dac89ce5a3e83a8bc5dd652a8e7270b26d232b6
Ignacio Gonzalez Sorribas <igonzalezsorribas@gmail.com>
2023-05-02 12:11:38 -0300

    Terminando la ruta de logger para testearla

--- src/controller/loggerTest.controller.js
+++ src/controller/loggerTest.controller.js
@@ -1,11 +1,11 @@
 const tesTer = (req, res) => {
-  req.logger.fatal('');
-  req.logger.error('');
-  req.logger.warn('');
-  req.logger.info('');
-  req.logger.http('');
-  req.logger.debug('');
-  res.json({ Error });
+  req.logger.fatal('Este es un mensaje de error fatal');
+  req.logger.error('Este es un mensaje de error');
+  req.logger.warn('Este es un mensaje de advertencia');
+  req.logger.info('Este es un mensaje informativo');
+  req.logger.http('logger');
+  req.logger.debug('Este es un mensaje de depuración');
+  res.json({ logger });
 };
 
 module.exports = tesTer;
