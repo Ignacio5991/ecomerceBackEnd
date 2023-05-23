424daaf379e65a9b936750b3ae44add2286f0cfe
Ignacio Gonzalez Sorribas <igonzalezsorribas@gmail.com>
2023-05-23 17:35:35 -0300

    Creando la documentacion del carrito

--- src/dao/models/products.model.js
+++ src/dao/models/products.model.js
@@ -32,19 +32,6 @@ const productSchema = new mongoose.Schema({
     type: String,
     default: 'admin',
   },
-
-  // owner: {
-  //   type: [
-  //     {
-  //       reference: {
-  //         type: mongoose.Schema.Types.ObjectId,
-  //         ref: 'users',
-  //         enum: ['admin', 'premium'],
-  //       },
-  //     },
-  //   ],
-  //   default: ['admin' || 'premium'],
-  // },
 });
 productSchema.plugin(paginate);
 const productModel = mongoose.model('products', productSchema);
