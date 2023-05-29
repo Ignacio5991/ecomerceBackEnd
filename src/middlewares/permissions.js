bc67010e643226a44c1c678a8caf1c4923d43430
Ignacio Gonzalez Sorribas <igonzalezsorribas@gmail.com>
2023-05-29 19:00:37 -0300

    Testing de carrito 100%

--- src/middlewares/permissions.js
+++ src/middlewares/permissions.js
@@ -1,7 +1,7 @@
 const adminPermission = async (req, res, next) => {
   console.log(req.user.role);
   if (req.user.role !== 'admin' && req.user.role !== 'premium') {
-    return res.status(401).json({
+    return res.status(402).json({
       status: 'error',
       msg: 'Usuario no autorizado ',
     });
@@ -11,13 +11,20 @@ const adminPermission = async (req, res, next) => {
 
 const userPermission = async (req, res, next) => {
   console.log(req.session.user);
-  if (!req.session.user || req.session?.user?.role !== 'user') {
+  if (!req.session.user) {
     return res.status(401).json({
       status: 'error',
-      msg: 'Usuario no autorizado',
+      msg: 'error usuario no autorizado',
     });
   }
   next();
+  // if (!req.session.user || req.session?.user?.role !== 'user') {
+  //   return res.status(401).json({
+  //     status: 'error',
+  //     msg: 'Usuario no autorizado',
+  //   });
+  // }
+  // next();
 };
 const premiumPermission = async (req, res, next) => {
   if (req.user.role !== 'admin' || req.user.role !== 'premium') {
