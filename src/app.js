ee54850a5110815882bf709862bffd6293c85a27
Ignacio Gonzalez Sorribas <igonzalezsorribas@gmail.com>
2023-07-02 21:37:59 -0300

    Fixed

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ const httpServer = server.listen(PORT, () => console.log('Servidor listo escucha
 server.use(
   cors({
     credentials: true,
-    origin: 'http://frontend-desafiofinal-production.up.railway.app',
+    origin: 'https://frontend-desafiofinal-production.up.railway.app',
   })
 );
 
